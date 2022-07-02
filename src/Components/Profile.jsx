278d7a82ac8af8dbf653ddd6d48a0ed7a063ade3
Kenan <haciyevk785@gmail.com>
2022-07-02 21:25:27 +0400

    first steps in design and dividing on modules

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://www.prior.com/wp-content/uploads/2021/03/QG_brochure_blog_-875x350.jpg"
+          alt="bg"
+        />
+      </div>
+      <div> ava + description</div>
+      <div>
+        my posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
